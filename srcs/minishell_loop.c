9ddafd91295c83a339c0690f3f5e63b3afd77fac
Louis Brochard <louisbrochard@macbook-air-de-louis-4.home>
2023-06-21 19:34:22 +0200

    21juin2023

--- srcs/minishell_loop.c
+++ srcs/minishell_loop.c
@@ -6,7 +6,7 @@
 /*   By: louisbrochard <louisbrochard@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/21 12:26:51 by louisbrocha       #+#    #+#             */
-/*   Updated: 2023/06/21 12:26:53 by louisbrocha      ###   ########.fr       */
+/*   Updated: 2023/06/21 18:41:21 by louisbrocha      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,15 @@ int ft_reset(t_tools *tools)
 
 int minishell_loop(t_tools *tools)
 {
+    char *tmp;
+
     tools->args = readline("minishell> ");
-    printf("OK\n");
-    if (!ft_strncmp(tools->args, "exit",4))
-    {
-        printf("exit\n");
+    tmp = ft_strtrim(tools->args, " ");
+    free(tools->args);
+    tools->args = tmp;
+    if (!ft_strncmp(tools->args, "exit", ft_strlen(tools->args)) && tools->args[0] != '\0')
         exit(0);
-    }
+    add_history(tools->args);
     ft_reset(tools);
     return (0);
 }
