5ae7cec66b70256840a98b20a7c828b9bc479119
Louis BROCHARD <lbrochar@paul-f3Br10s1.clusters.42paris.fr>
2023-06-29 11:28:35 +0200

    29juin2023

--- srcs/ft_token_reader_utils.c
+++ srcs/ft_token_reader_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_token_reader_utils.c                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: louisbrochard <louisbrochard@student.42    +#+  +:+       +#+        */
+/*   By: lbrochar <lbrochar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/28 21:52:30 by louisbrocha       #+#    #+#             */
-/*   Updated: 2023/06/28 23:20:44 by louisbrocha      ###   ########.fr       */
+/*   Updated: 2023/06/29 11:27:43 by lbrochar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,18 +20,18 @@ t_lexer	*ft_lexernew(char *str, int token)
 	if (!new_element)
 		return (0);
 	new_element->type = token;
-    if (token == 1)
-        new_element->content = ft_strdup("|");
-    else if (token == 2)
-        new_element->content = ft_strdup(">>");
-    else if (token == 3)
-        new_element->content = ft_strdup("<<");
-    else if (token == 4)
-        new_element->content = ft_strdup(">");
-    else if (token == 5)
-        new_element->content = ft_strdup("<");
-    else if (token == 6)
-        new_element->content = ft_strdup(str);
+	if (token == 1)
+		new_element->content = ft_strdup("|");
+	if (token == 2)
+		new_element->content = ft_strdup(">>");
+	if (token == 3)
+		new_element->content = ft_strdup("<<");
+	if (token == 4)
+		new_element->content = ft_strdup(">");
+	if (token == 5)
+		new_element->content = ft_strdup("<");
+	if (token == 6)
+		new_element->content = ft_strdup(str);
 	new_element->next = NULL;
 	return (new_element);
 }
@@ -69,16 +69,16 @@ int	handle_quotes(int i, char *str, char del)
 
 int	check_token(char *c)
 {
-    if (c[0] == '|')
-        return (1);
-    else if (c[0] == '>' && c[1] == '>')
-        return (2);
-    else if (c[0] == '<' && c[1] == '<')
-        return (3);
-    else if (c[0] == '>' && c[1] != '>')
-        return (4);
-    else if (c[0] == '<' && c[1] != '<')
-        return (5);
+	if (c[0] == '|')
+		return (1);
+	else if (c[0] == '>' && c[1] == '>')
+		return (2);
+	else if (c[0] == '<' && c[1] == '<')
+		return (3);
+	else if (c[0] == '>' && c[1] != '>')
+		return (4);
+	else if (c[0] == '<' && c[1] != '<')
+		return (5);
 	return (0);
 }
 
